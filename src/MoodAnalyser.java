005fcc1545224e1c307ea471925c3fbc9b05d0b7
Your Name <you@example.com>
2022-07-12 19:47:00 +0530

    refactored the code

---
+++ src/MoodAnalyser.java
@@ -0,0 +1,18 @@
+
+public class MoodAnalyser {
+
+	private String msg;
+    public MoodAnalyser() {
+    }
+
+    public MoodAnalyser(String msg){
+        this.msg = msg;
+    }
+
+    public String analyzeMood() {
+        if(msg.contains("Sad")){
+            return "Sad";
+        }
+        return "Happy";
+    }
+}
